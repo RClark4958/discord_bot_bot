754da54808ab8f04e6604bf222e9fcff60f2cdcf
Rick Clark <rickdclark48@gmail.com>
2023-10-11 16:03:47 -0600

    feat: make calls async

--- bot5.py
+++ bot5.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import openai
-import requests
+import aiohttp  # <-- Imported for async HTTP
 from dotenv import load_dotenv
 
 # Load the Discord bot token and OpenAI API key from environment variables
@@ -27,25 +27,26 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def chatgpt_query(prompt):
-    data = {
-        "model": "gpt-4",
-        "messages": [
-            {"role": "system", "content": "You are a helpful and cheery devops and software engineering expert."},
-            {"role": "user", "content": prompt}
-        ],
-        "max_tokens": 8000,
-        "temperature": 0.8,
-        "top_p": 1,
-        "frequency_penalty": 0,
-        "presence_penalty": 0
-    }
-    response = requests.post(chatgpt_url, json=data, headers=headers)
-    response_json = response.json()
-    if "choices" in response_json:
-        answer = response_json["choices"][0]["message"]["content"].strip()
-    else:
-        answer = "I'm sorry, I couldn't generate an answer. Please try again later"
-    return answer
+    async with aiohttp.ClientSession() as session:
+        data = {
+            "model": "gpt-4",
+            "messages": [
+                {"role": "system", "content": "You are a helpful and cheery devops and software engineering expert."},
+                {"role": "user", "content": prompt}
+            ],
+            "max_tokens": 8000,
+            "temperature": 0.8,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0
+        }
+        async with session.post(chatgpt_url, json=data, headers=headers) as response:
+            response_json = await response.json()
+            if "choices" in response_json:
+                answer = response_json["choices"][0]["message"]["content"].strip()
+            else:
+                answer = "I'm sorry, I couldn't generate an answer. Please try again later"
+            return answer
 
 @bot.event
 async def on_ready():
@@ -54,13 +55,15 @@ async def on_ready():
 @bot.command(name="info", help="Ask advice, strategy, information for any game")
 async def info(ctx, *, question):
     answer = await chatgpt_query(question)
-    await ctx.send(answer)
+    
+    # Split answer into chunks for Discord's 2000 character limit
+    for chunk in [answer[i:i + 1990] for i in range(0, len(answer), 1990)]:
+        await ctx.send(chunk)
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(f"Unknown command. Please use the correct command prefix. For example: `!info <your_question>`.")
-
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
